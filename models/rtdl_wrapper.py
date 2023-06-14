34ad90a398d7557ada4bace805906422bbf63d58
roeimichael <roeym111@gmail.com>
2023-06-14 16:15:42 +0300

    added functionality for testing models in rtdl

--- models/rtdl_wrapper.py
+++ models/rtdl_wrapper.py
@@ -4,21 +4,55 @@ import torch
 import torch.nn as nn
 
 
+# class Model(nn.Module):
+#     def __init__(
+#         self,
+#         n_num_features: int,
+#         cat_tokenizer: rtdl.CategoricalFeatureTokenizer,
+#         mlp_kwargs: Dict[str, Any],
+#     ):
+#         super().__init__()
+#         self.cat_tokenizer = cat_tokenizer
+#         self.model = rtdl.MLP.make_baseline(
+#             d_in=n_num_features + cat_tokenizer.n_tokens * cat_tokenizer.d_token,
+#             **mlp_kwargs,
+#         )
+#
+#     def forward(self, x_num, x_cat):
+#         return self.model(
+#             torch.cat([x_num, self.cat_tokenizer(x_cat).flatten(1, -1)], dim=1)
+#         )
+class ResidualBlock(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+        self.fc1 = nn.Linear(num_features, num_features)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(num_features, num_features)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        residual = x
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return residual + x
+
+
 class Model(nn.Module):
-    def __init__(
-        self,
-        n_num_features: int,
-        cat_tokenizer: rtdl.CategoricalFeatureTokenizer,
-        mlp_kwargs: Dict[str, Any],
-    ):
+    def __init__(self, n_num_features, cat_tokenizer, mlp_kwargs):
         super().__init__()
         self.cat_tokenizer = cat_tokenizer
-        self.model = rtdl.MLP.make_baseline(
-            d_in=n_num_features + cat_tokenizer.n_tokens * cat_tokenizer.d_token,
-            **mlp_kwargs,
-        )
+        d_layers = mlp_kwargs.get('d_layers', [256, 256, 256])
+        dropout = mlp_kwargs.get('dropout', 0.1)
+        d_out = mlp_kwargs.get('d_out', 1)
+
+        self.fc1 = nn.Linear(n_num_features + cat_tokenizer.n_tokens * cat_tokenizer.d_token, d_layers[0])
+        self.res_blocks = nn.Sequential(*[ResidualBlock(size) for size in d_layers])
+        self.fc2 = nn.Linear(d_layers[-1], d_out)
 
     def forward(self, x_num, x_cat):
-        return self.model(
-            torch.cat([x_num, self.cat_tokenizer(x_cat).flatten(1, -1)], dim=1)
-        )
+        x = torch.cat([x_num, self.cat_tokenizer(x_cat).flatten(1, -1)], dim=1)
+        x = self.fc1(x)
+        x = self.res_blocks(x)
+        return self.fc2(x)
--- rtdl_nets.py
+++ rtdl_nets.py
@@ -13,11 +13,13 @@ import zero
 from CSVDataset_rtdl import CSVDataset_rtdl
 from torch.utils.data import DataLoader, TensorDataset
 from models.rtdl_wrapper import Model
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import warnings
+warnings.filterwarnings('ignore')
 
 device = torch.device('cpu')
 
 zero.improve_reproducibility(seed=123456)
-
 
 def prepare_data(path_train, path_test, train_path, starting_date):
     dataset = CSVDataset_rtdl(path_train, path_test, train_path, starting_date)
@@ -33,57 +35,26 @@ def prepare_data(path_train, path_test, train_path, starting_date):
     return data, dataset.cardinalities_train, dataset.cardinalities_test
 
 
-# creates dates list
 with open('./data/dates.txt', 'r') as fp:
     dates = [line.strip() for line in fp.readlines()]
 
-# first date to check
 starting_date = 466
-
-# fixing paths to correct folders
 path_train = f'./data/dates1.5/{dates[starting_date]}.csv'
 path_test = f'./data/dates1.5/{dates[starting_date + 1]}.csv'
 train_path = './data/Targets1.5.csv'
 next_day_data_path = f'./data/dates1.5/{dates[starting_date + 2]}.csv'
 
-# Prepare the data using the function defined above
 data, cardinalities_train, cardinalities_test = prepare_data(path_train, path_test, train_path, starting_date)
 train_dataset = TensorDataset(*data['train'])
 train_dl = DataLoader(train_dataset, batch_size=32, shuffle=True)
-
 test_dataset = TensorDataset(*data['test'])
 test_dl = DataLoader(test_dataset, batch_size=1024, shuffle=False)
 
-# Determine the number of inputs based on the size of the training data
 n_inputs = data['train'][0].shape[1]
 d_token, d_out = 2, 1
-# creates the model
-model = Model(
-    n_inputs,
-    rtdl.CategoricalFeatureTokenizer(cardinalities_train, d_token, True, 'uniform'),
-    {
-        "d_layers": [128, 256, 128],
-        "dropout": 0.1,
-        "d_out": d_out
-    })
 
-# setting the parameters for the calculation
-lr = 0.001
-weight_decay = 0.0
-model.to(device)
-optimizer = (
-    model.make_default_optimizer()
-    if isinstance(model, rtdl.FTTransformer)
-    else torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
-)
-loss_fn = (
-    F.binary_cross_entropy_with_logits
-)
-
-
-# Define an evaluation function to calculate accuracy on a given dataset part (e.g., 'test' or 'val')
 @torch.no_grad()
-def evaluate(dataloader):
+def evaluate(model, dataloader):
     model.eval()
     prediction = []
     for num_features, cat_features, targets in dataloader:
@@ -92,34 +63,122 @@ def evaluate(dataloader):
     prediction = torch.cat(prediction).cpu().numpy()
     target = dataloader.dataset.tensors[2].cpu().numpy().flatten()
     prediction = scipy.special.expit(prediction)
-    prediction = np.round(prediction)  # Round off to get the binary predictions
-    score = sklearn.metrics.accuracy_score(target, prediction)
-    return score
+    prediction = np.round(prediction)
+
+    accuracy = accuracy_score(target, prediction)
+    precision = precision_score(target, prediction)
+    recall = recall_score(target, prediction)
+    f1 = f1_score(target, prediction)
+    confusion = confusion_matrix(target, prediction)
+
+    return accuracy, precision, recall, f1, confusion
+
+def train_and_evaluate(layers, layer_size, dropout, lr, weight_decay):
+    model = Model(
+        n_inputs,
+        rtdl.CategoricalFeatureTokenizer(cardinalities_train, d_token, True, 'uniform'),
+        {"d_layers": [layer_size] * layers, "dropout": dropout, "d_out": d_out})
+    model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+    loss_fn = F.binary_cross_entropy_with_logits
+
+    batch_size = 256
+    train_loader = zero.data.IndexLoader(len(train_dl), batch_size, device=device)
+    progress = zero.ProgressTracker(patience=100)
+    n_epochs = 100
+
+    print(f'Training Model with {layers} layers of size {layer_size}, dropout {dropout}, learning rate {lr}, weight decay {weight_decay}')
+    for epoch in range(1, n_epochs + 1):
+        for iteration, (num_features, cat_features, targets) in enumerate(train_dl):
+            model.train()
+            optimizer.zero_grad()
+            output = model(num_features, cat_features)
+            loss = loss_fn(output, targets)
+            loss.backward()
+            optimizer.step()
+
+        final_metrics = evaluate(model, test_dl)
+        progress.update(final_metrics[0])
+        if progress.fail:
+            break
+
+    final_metrics = evaluate(model, test_dl)
+    return final_metrics
+
+layers_to_test = [3, 5, 7, 10]
+layer_sizes_to_test = [64, 128, 256]
+dropout_to_test = [0.1, 0.3, 0.5]
+lr_to_test = [0.001, 0.01]
+weight_decay_to_test = [0.0, 0.1, 0.2]
+
+results_df = pd.DataFrame()
+
+for layers in layers_to_test:
+    for layer_size in layer_sizes_to_test:
+        for dropout in dropout_to_test:
+            for lr in lr_to_test:
+                for weight_decay in weight_decay_to_test:
+                    final_metrics = train_and_evaluate(layers, layer_size, dropout, lr, weight_decay)
+
+                    new_results = pd.DataFrame({
+                        'Model': ['ResNet'],
+                        'Layers': [layers],
+                        'Layer Size': [layer_size],
+                        'Dropout': [dropout],
+                        'Learning Rate': [lr],
+                        'Weight Decay': [weight_decay],
+                        'Accuracy': [final_metrics[0]],
+                        'Precision': [final_metrics[1]],
+                        'Recall': [final_metrics[2]],
+                        'F1 Score': [final_metrics[3]],
+                        'Confusion Matrix': [final_metrics[4]]
+                    })
+
+                    results_df = pd.concat([results_df, new_results], ignore_index=True)
+                    results_df.to_csv('model_results.csv', index=False)
+
+# Determine the number of inputs based on the size of the training data
+# n_inputs = data['train'][0].shape[1]
+# d_token, d_out = 2, 1
+# # creates the model
+# model = Model(
+#     n_inputs,
+#     rtdl.CategoricalFeatureTokenizer(cardinalities_train, d_token, True, 'uniform'),
+#     {
+#         "d_layers": [256]*10,  # Ten layers each of size 256
+#         "dropout": 0.1,
+#         "d_out": d_out
+#     })
+
+# setting the parameters for the calculation
+# lr = 0.001
+# weight_decay = 0.0
+# model.to(device)
+# optimizer = (
+#     model.make_default_optimizer()
+#     if isinstance(model, rtdl.FTTransformer)
+#     else torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+# )
+# loss_fn = (
+#     F.binary_cross_entropy_with_logits
+# )
 
 
-batch_size = 256
-train_loader = zero.data.IndexLoader(len(train_dl), batch_size, device=device)
-progress = zero.ProgressTracker(patience=100)
-print(f'Test score before training: {evaluate(test_dl):.4f}')
-n_epochs = 100
-report_frequency = 5
+# Define an evaluation function to calculate accuracy on a given dataset part (e.g., 'test' or 'val')
 
-for epoch in range(1, n_epochs + 1):
-    for iteration, (num_features, cat_features, targets) in enumerate(train_dl):
-        model.train()
-        optimizer.zero_grad()
-        output = model(num_features, cat_features)
-        loss = loss_fn(output, targets)
-        loss.backward()
-        optimizer.step()
-        if iteration % report_frequency == 0:
-            print(f'(epoch) {epoch} (batch) {iteration} (loss) {loss.item():.4f}')
 
-    test_score = evaluate(test_dl)
-    print(f'Epoch {epoch:03d} | Test score: {test_score:.4f}', end='')
-    progress.update(test_score)
-    if progress.success:
-        print(' <<< BEST EPOCH', end='')
-    print()
-    if progress.fail:
-        break
+# new_results = pd.DataFrame({
+#     'Model': ['ResNet'],
+#     'Layers': [10],
+#     'Layer Size': [256],
+#     'Dropout': [0.1],
+#     'Learning Rate': [lr],
+#     'Weight Decay': [weight_decay],
+#     'Accuracy': [final_metrics[0]],
+#     'Precision': [final_metrics[1]],
+#     'Recall': [final_metrics[2]],
+#     'F1 Score': [final_metrics[3]],
+#     'Confusion Matrix': [final_metrics[4]]
+# })
+#
+# results_df = pd.concat([results_df, new_results], ignore_index=True)
