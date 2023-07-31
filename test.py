acf116792b3e96ae7a4ba0317a699e9e795bbfab
roeimichael <roeym111@gmail.com>
2023-07-31 21:08:10 +0300

    added a print statement to the test file to see if the problem is with the gpu of the server

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import torch
-
+print("hello there")
+if(torch.cuda.is_available()):
+    print("cuda is available")
 a = torch.tensor([1.0, 2.0], device="cuda" if torch.cuda.is_available() else "cpu")
 print(a)
